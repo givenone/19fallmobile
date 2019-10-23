749e46b0834a5aca3101db7243ba6ddeac9f3647
paramecium6 <paramecium6@gmail.com>
2019-10-23 16:11:22 +0900

    git ignore ㅋㅋ

--- ezorder_backend/ezorder_backend/settings.py
+++ ezorder_backend/ezorder_backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Menu.apps.MenuConfig',
+    'Order.apps.OrderConfig',
+    'User.apps.UserConfig',
+    'Store.apps.StoreConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
