40d8eb28b693f562c083017663d5eb146d5ee4ba
wookjeHan <gksdnrwp@naver.com>
2019-11-04 17:57:12 +0900

    Add about Signing layout

--- ezorder_app/app/src/main/java/com/example/ezorder/MainActivity.kt
+++ ezorder_app/app/src/main/java/com/example/ezorder/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.first_page_app)
+        setContentView(R.layout.activity_sign_select_su)
     }
     //TODO 화면전환 및 백엔드와의 request 통신 결과 띄우기
 }
