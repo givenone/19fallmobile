4e6bb5ee8d1156f46fa038cf53e0d81403f39fac
wookjeHan <gksdnrwp@naver.com>
2019-11-04 16:16:07 +0900

    Add first_Page_APP Layout

--- ezorder_app/app/src/main/java/com/example/ezorder/MainActivity.kt
+++ ezorder_app/app/src/main/java/com/example/ezorder/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.whenorder)
+        setContentView(R.layout.first_page_app)
     }
     //TODO 화면전환 및 백엔드와의 request 통신 결과 띄우기
 }
