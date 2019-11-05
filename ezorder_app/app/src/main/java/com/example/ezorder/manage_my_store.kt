80489f8a5b7ac13fe593bac3628f7fe1c7438480
Park Yun Bin <parkyunbin@gmail.com>
2019-11-06 03:57:31 +0900

    Main page designed, others unfinished

--- ezorder_app/app/src/main/java/com/example/ezorder/manage_my_store.kt
+++ ezorder_app/app/src/main/java/com/example/ezorder/manage_my_store.kt
@@ -17,7 +17,7 @@ class manage_my_store : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manage_my_store, container, false)
+        return inflater.inflate(R.layout.fragment_edit_my_store, container, false)
     }
 
     companion object {
