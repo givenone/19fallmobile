e54732135ff40871770afb00b159e6d527f57880
paramecium6 <paramecium6@gmail.com>
2019-12-05 21:22:04 +0900

    You mean this?

--- ezorder_app/app/src/main/java/com/example/ezorder/VolleyService.kt
+++ ezorder_app/app/src/main/java/com/example/ezorder/VolleyService.kt
@@ -15,7 +15,7 @@ import org.json.JSONObject
 
 object VolleyService {
     var token = ""
-    val testUrl = "http://192.168.56.1:8000/"
+    val testUrl = "http://10.8.28.17:8000/"
 
     fun GETVolley(context: Context, url : String, thetoken : String, res: (Boolean, String) -> Unit) {
 
